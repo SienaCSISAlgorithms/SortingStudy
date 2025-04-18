93b26cf5ac62346e7ffc952a30158bf78f2b8140
jteresco <terescoj@gmail.com>
2025-04-18 09:18:26 -0400

    Bubble Sort implementation with Copilot

---
+++ BubbleSort.java
@@ -0,0 +1,53 @@
+/**
+ * Implementation of the Bubble Sort algorithm that extends the SortingAlgorithm
+ * abstract class. This class performs a straightforward iterative bubble sort
+ * while counting the number of comparisons and swaps.
+ * 
+ * @author Jim Teresco via Copilot
+ * @version Spring 2025
+ */
+public class BubbleSort extends SortingAlgorithm {
+
+    /**
+     * Constructor for the BubbleSort class.
+     * 
+     * @param array the array to be sorted
+     */
+    public BubbleSort(int[] array) {
+        super(array);
+    }
+
+    /**
+     * Performs the Bubble Sort algorithm on the array.
+     */
+    @Override
+    public void sort() {
+        long startTime = System.nanoTime(); // Start timing the sort
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                comparisons++; // Increment comparison count
+                if (array[j] > array[j + 1]) {
+                    // Swap elements
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swaps++; // Increment swap count
+                }
+            }
+        }
+
+        long endTime = System.nanoTime(); // End timing the sort
+        time = endTime - startTime; // Calculate total time taken
+    }
+
+    /**
+     * Returns the name of the sorting algorithm.
+     * 
+     * @return the name of the sorting algorithm
+     */
+    @Override
+    public String getName() {
+        return "bubble";
+    }
+}
